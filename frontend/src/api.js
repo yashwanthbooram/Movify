851212969b28d150868b9f191b42ec49cfd5d55c
yashwanthbooram <booramyashwanth@gmail.com>
2025-07-31 22:56:31 +0530

    Final: Configure frontend for live deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://movify-backend.onrender.com' // <-- YOUR LIVE RENDER URL
+  baseURL: 'https://movify-backend.onrender.com'
 });
 
 export default API;
